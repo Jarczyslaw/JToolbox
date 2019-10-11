80c24ec803fd5e7532e8c17741bee4cb212105cc
Jarczyslaw <tyma.jaroslaw@gmail.com>
2019-10-11 11:21:30 +0200

    dev

---
+++ XamarinForms/JToolbox.XamarinForms.Logging/ILoggerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JToolbox.XamarinForms.Logging
+{
+    public interface ILoggerService
+    {
+        void Debug(string message, params object[] args);
+        void Error(Exception exception);
+        void Error(Exception exception, string message, params object[] args);
+        void Error(string message, params object[] args);
+        void Fatal(Exception exception);
+        void Fatal(Exception exception, string message, params object[] args);
+        void Fatal(string message, params object[] args);
+        void Info(string message, params object[] args);
+        void Trace(string message, params object[] args);
+        void Warn(string message, params object[] args);
+    }
+}
