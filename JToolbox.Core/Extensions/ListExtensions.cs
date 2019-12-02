ad073bde773fe08133ca0a226e8314079697078a
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-12-02 18:58:08 +0100

    dev

--- JToolbox.Core/Extensions/ListExtensions.cs
+++ JToolbox.Core/Extensions/ListExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace JToolbox.Core.Extensions
 {
@@ -23,19 +24,32 @@ namespace JToolbox.Core.Extensions
         public static List<List<T>> ChunkInto<T>(this List<T> list, int chunksCount)
         {
             var result = new List<List<T>>();
+            if (chunksCount > list.Count)
+            {
+                chunksCount = list.Count;
+            }
             var chunkSize = (float)list.Count / chunksCount;
             if (chunkSize < 1f)
             {
                 chunkSize = 1f;
             }
 
-            var itemsAdded = 0;
             for (int i = 0; i < chunksCount; i++)
             {
                 var index = (int)Math.Round(i * chunkSize);
-                var itemsToAdd = (int)Math.Round((i + 1) * chunkSize) - itemsAdded;
+                var itemsToAdd = (int)Math.Round((i + 1) * chunkSize) - index;
                 result.Add(list.GetRange(index, itemsToAdd));
-                itemsAdded += itemsToAdd;
+            }
+            return result;
+        }
+
+        public static List<T> Peek<T>(this List<T> list, int count)
+        {
+            var result = list.Take(count)
+                .ToList();
+            if (result.Count > 0)
+            {
+                list.RemoveRange(0, result.Count());
             }
             return result;
         }
---
+++ JToolbox.Core/Extensions/MiscExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace JToolbox.Core.Extensions
+{
+    public static class MiscExtensions
+    {
+        public static bool In<T>(this T val, params T[] values) where T : struct
+        {
+            return values.Contains(val);
+        }
+    }
+}
