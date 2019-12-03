ca5a4028c5a6a2995e47b50a0df93baad9842ab4
Jarczyslaw <tyma.jaroslaw@gmail.com>
2019-12-03 13:20:00 +0100

    dev

--- JToolbox.Core/Helpers/AsyncHelper.cs
+++ JToolbox.Core/Helpers/AsyncHelper.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -25,5 +28,37 @@ namespace JToolbox.Core.Helpers
                 .Unwrap()
                 .GetAwaiter()
                 .GetResult();
+
+        public static Task ForEach<TItem>(IEnumerable<TItem> input, Func<TItem, Task> handler)
+        {
+            return ForEach(input, handler, CancellationToken.None);
+        }
+
+        public static async Task ForEach<TItem>(IEnumerable<TItem> input, Func<TItem, Task> handler, CancellationToken cancellationToken)
+        {
+            var tasks = new List<Task>();
+            foreach (var item in input)
+            {
+                tasks.Add(Task.Run(async () => await handler(item), cancellationToken));
+            }
+            await Task.WhenAll(tasks);
+        }
+
+        public static Task<IEnumerable<KeyValuePair<TItem, TResult>>> ForEach<TItem, TResult>(IEnumerable<TItem> input, Func<TItem, Task<TResult>> handler)
+        {
+            return ForEach(input, handler, CancellationToken.None);
+        }
+
+        public static async Task<IEnumerable<KeyValuePair<TItem, TResult>>> ForEach<TItem, TResult>(IEnumerable<TItem> input, Func<TItem, Task<TResult>> handler, CancellationToken cancellationToken)
+        {
+            var result = new BlockingCollection<KeyValuePair<TItem, TResult>>();
+            var tasks = new List<Task>();
+            foreach (var item in input)
+            {
+                tasks.Add(Task.Run(async () => result.Add(new KeyValuePair<TItem, TResult>(item, await handler(item))), cancellationToken));
+            }
+            await Task.WhenAll(tasks);
+            return result.ToList();
+        }
     }
 }
