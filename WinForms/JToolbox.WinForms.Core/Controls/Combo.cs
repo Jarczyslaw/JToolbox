3ea6083313f4586510fa332cb0170375fb7d1b97
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-12-07 21:36:16 +0100

    dev

---
+++ WinForms/JToolbox.WinForms.Core/Controls/Combo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace JToolbox.WinForms.Core.Controls
+{
+    public delegate void OnComboItemChanged<T>(T value);
+
+    public class Combo<T> : ComboBox
+    {
+        public event OnComboItemChanged<T> OnComboItemChanged = delegate { };
+
+        public Combo()
+        {
+            DropDownStyle = ComboBoxStyle.DropDownList;
+            DisplayMember = nameof(ComboItem<T>.Header);
+            ValueMember = nameof(ComboItem<T>.Value);
+            SelectedValueChanged += Combo_SelectedValueChanged;
+        }
+
+        public List<ComboItem<T>> ComboItems
+        {
+            get => DataSource as List<ComboItem<T>>;
+            set
+            {
+                DataSource = null;
+                DataSource = value;
+                SelectedValue = value.FirstOrDefault();
+            }
+        }
+
+        public T SelectedComboItem
+        {
+            get => (T)SelectedValue;
+            set => SelectedValue = value;
+        }
+
+        private void Combo_SelectedValueChanged(object sender, EventArgs e)
+        {
+            OnComboItemChanged(SelectedComboItem);
+        }
+    }
+}
---
+++ WinForms/JToolbox.WinForms.Core/Controls/ComboItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JToolbox.WinForms.Core.Controls
+{
+    public class ComboItem<T>
+    {
+        public string Header { get; set; }
+        public T Value { get; set; }
+    }
+}
