2da610d27bc85227628c1ef59781d7b4fa8f6b68
Jarczyslaw <tyma.jaroslaw@gmail.com>
2020-02-06 13:49:52 +0100

    dev

---
+++ Examples/WCF/WCFExample.Common/ITestProxy.cs
@@ -0,0 +1,12 @@
+﻿using JToolbox.WCF;
+using System.ServiceModel;
+
+namespace WCFExample.Common
+{
+    [ServiceContract]
+    public interface ITestProxy : IProxy
+    {
+        [OperationContract]
+        string Ping(string message);
+    }
+}
