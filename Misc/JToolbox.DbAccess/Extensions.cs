c7830ea5c45fe8410f04910cd0bf5d7cae884bb3
Jarczyslaw <tyma.jaroslaw@gmail.com>
2020-04-07 22:34:54 +0200

    added JToolbox.DbAccess

---
+++ Misc/JToolbox.DbAccess/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+
+namespace JToolbox.DbAccess
+{
+    public static class Extensions
+    {
+        public static void AddParameter(this IDbCommand command, string name, DbType type, object value)
+        {
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = name;
+            parameter.DbType = type;
+            parameter.Value = value;
+            command.Parameters.Add(parameter);
+        }
+
+        public static DataSet ExecuteDataSet(this IDbCommand command, IDbDataAdapter adapter)
+        {
+            var ds = new DataSet();
+            adapter.SelectCommand = command;
+            adapter.Fill(ds);
+            return ds;
+        }
+
+        public static DataTable ExecuteDataTable(this IDbCommand command, IDbDataAdapter adapter)
+        {
+            return command.ExecuteDataSet(adapter).Tables[0];
+        }
+    }
+}
---
+++ Misc/JToolbox.DbAccess/IDbFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace JToolbox.DbAccess
+{
+    public interface IDbFactory
+    {
+        IDbConnection CreateConnection();
+
+        IDbDataAdapter GetDataAdapter();
+    }
+}
