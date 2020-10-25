d8e90034d3100e95ebb4461dfbc18f61c9e3b7a4
Jarczyslaw <tyma.jaroslaw@gmail.com>
2020-10-25 22:22:53 +0100

    add TasksExecutorConsoleExample

---
+++ _Examples/TasksExecutor/TasksExecutorConsoleExample/ConsoleTask.cs
@@ -0,0 +1,26 @@
+﻿using JToolbox.Threading.TasksExecution;
+using System;
+using System.Threading;
+
+namespace TasksExecutorConsoleExample
+{
+    public class ConsoleTask : BaseTask
+    {
+        private static int taskId;
+
+        public int TaskId { get; set; }
+
+        public ConsoleTask()
+        {
+            taskId++;
+            TaskId = taskId;
+        }
+
+        public override void Run()
+        {
+            Console.WriteLine($"Task {TaskId} started");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Task {TaskId} stopped");
+        }
+    }
+}
---
+++ _Examples/TasksExecutor/TasksExecutorConsoleExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using JToolbox.Threading.TasksExecution;
+using System;
+
+namespace TasksExecutorConsoleExample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var executor = new TasksExecutor
+            {
+                ThreadsTimeout = TimeSpan.FromSeconds(2)
+            };
+            executor.OnTasksExecutorStateChanged += Executor_OnTasksExecutorStateChanged;
+            for (int i = 0; i < 5; i++)
+            {
+                executor.Add(new ConsoleTask());
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Executor_OnTasksExecutorStateChanged(TasksExecutorState state)
+        {
+            Console.WriteLine($"Threads: {state.Threads}, working: {state.WorkingThreads}, idle: {state.IdleThreads}, waiting: {state.WaitingThreads}, pending tasks: {state.PendingTasks}");
+        }
+    }
+}
