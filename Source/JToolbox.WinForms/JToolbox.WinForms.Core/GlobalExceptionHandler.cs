a2d3dda8af6318ad4ac6032a2fdb5946d95b3bcb
Jarczyslaw <tyma.jaroslaw@gmail.com>
2021-06-01 13:49:12 +0200

    WinForms update

---
+++ Source/JToolbox.WinForms/JToolbox.WinForms.Core/GlobalExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace JToolbox.WinForms.Core
+{
+    public abstract class GlobalExceptionHandler
+    {
+        public void Attach()
+        {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            HandleException((Exception)e.ExceptionObject, nameof(CurrentDomain_UnhandledException));
+        }
+
+        private void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            HandleException(e.Exception, nameof(Application_ThreadException));
+        }
+
+        protected abstract void HandleException(Exception exception, string source);
+    }
+}
