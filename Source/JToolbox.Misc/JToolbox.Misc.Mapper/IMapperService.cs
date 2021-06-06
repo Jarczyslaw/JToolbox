c3c7968cc0c52eabb34c6809e3a93923f110214a
Jarczyslaw <tyma.jaroslaw@gmail.com>
2021-06-06 20:24:42 +0200

    add mapper service

---
+++ Source/JToolbox.Misc/JToolbox.Misc.Mapper/IMapperService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace JToolbox.Misc.Mapper
+{
+    public interface IMapperService
+    {
+        void Bind<TIn, TOut>();
+
+        TOut Map<TIn, TOut>(TIn @in);
+
+        TOut Map<TIn, TOut>(TIn @in, TOut @out);
+
+        List<TOut> MapMany<TIn, TOut>(IEnumerable<TIn> @in);
+    }
+}
---
+++ Source/JToolbox.Misc/JToolbox.Misc.Mapper/MapperService.cs
@@ -0,0 +1,40 @@
+﻿using Nelibur.ObjectMapper;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JToolbox.Misc.Mapper
+{
+    public class MapperService : IMapperService
+    {
+        public void Bind<TIn, TOut>()
+        {
+            TinyMapper.Bind<TIn, TOut>();
+        }
+
+        public TOut Map<TIn, TOut>(TIn @in)
+        {
+            TryCreateBinding<TIn, TOut>();
+            return TinyMapper.Map<TOut>(@in);
+        }
+
+        public List<TOut> MapMany<TIn, TOut>(IEnumerable<TIn> @in)
+        {
+            TryCreateBinding<TIn, TOut>();
+            return @in.Select(s => Map<TIn, TOut>(s)).ToList();
+        }
+
+        public TOut Map<TIn, TOut>(TIn @in, TOut @out)
+        {
+            TryCreateBinding<TIn, TOut>();
+            return TinyMapper.Map(@in, @out);
+        }
+
+        private void TryCreateBinding<TIn, TOut>()
+        {
+            if (!TinyMapper.BindingExists<TIn, TOut>())
+            {
+                TinyMapper.Bind<TIn, TOut>();
+            }
+        }
+    }
+}
