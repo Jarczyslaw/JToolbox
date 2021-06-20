7da847c8677ea153caef4e6c16394e98a2c7e77c
Jarczyslaw <tyma.jaroslaw@gmail.com>
2021-06-20 18:52:10 +0200

    dev

---
+++ Source/JToolbox.Misc/JToolbox.Misc.Threading/ContextAwaiter.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace JToolbox.Misc.Threading
+{
+    public class ContextAwaiter
+    {
+        private readonly SynchronizationContext context;
+
+        public ContextAwaiter(SynchronizationContext context = null)
+        {
+            if (context == null)
+            {
+                context = SynchronizationContext.Current;
+            }
+            this.context = context;
+        }
+
+        public NotifyCompletion GetAwaiter()
+        {
+            return new NotifyCompletion(context);
+        }
+    }
+}
---
+++ Source/JToolbox.Misc/JToolbox.Misc.Threading/NotifyCompletion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace JToolbox.Misc.Threading
+{
+    public struct NotifyCompletion : INotifyCompletion
+    {
+        private static readonly SendOrPostCallback postCallback = state => ((Action)state)();
+
+        private readonly SynchronizationContext context;
+
+        public NotifyCompletion(SynchronizationContext context)
+        {
+            this.context = context;
+        }
+
+        public bool IsCompleted => context == SynchronizationContext.Current;
+
+        public void OnCompleted(Action continuation) => context.Post(postCallback, continuation);
+
+        public void GetResult()
+        {
+        }
+    }
+}
