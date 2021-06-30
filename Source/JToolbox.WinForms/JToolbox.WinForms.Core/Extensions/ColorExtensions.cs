fbcde031fd6c47f6fb6af70c984dda273f5c22e5
Jarczyslaw <tyma.jaroslaw@gmail.com>
2021-06-30 20:58:22 +0200

    dev

---
+++ Source/JToolbox.WinForms/JToolbox.WinForms.Core/Extensions/ColorExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace JToolbox.WinForms.Core.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color ChangeColorBrightness(this Color color, float correctionFactor)
+        {
+            var red = (float)color.R;
+            var green = (float)color.G;
+            var blue = (float)color.B;
+
+            if (correctionFactor < 0)
+            {
+                correctionFactor = 1 + correctionFactor;
+                red *= correctionFactor;
+                green *= correctionFactor;
+                blue *= correctionFactor;
+            }
+            else
+            {
+                red = (255 - red) * correctionFactor + red;
+                green = (255 - green) * correctionFactor + green;
+                blue = (255 - blue) * correctionFactor + blue;
+            }
+
+            return Color.FromArgb(color.A, (int)red, (int)green, (int)blue);
+        }
+    }
+}
