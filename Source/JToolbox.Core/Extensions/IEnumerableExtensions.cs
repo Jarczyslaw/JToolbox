8fd455826a724b3ab51f1ca36a1af314879b1925
Jarczyslaw <tyma.jaroslaw@gmail.com>
2021-07-22 13:18:29 +0200

    dev

--- Source/JToolbox.Core/Extensions/IEnumerableExtensions.cs
+++ Source/JToolbox.Core/Extensions/IEnumerableExtensions.cs
@@ -15,5 +15,29 @@ namespace JToolbox.Core.Extensions
         {
             return enumerable.Any() ? enumerable.Min(selector) : defaultValue;
         }
+
+        public static List<T> SearchRecursively<T>(this IEnumerable<T> @this, Func<T, IEnumerable<T>> childrenSelector, Predicate<T> predicate)
+        {
+            var result = new List<T>();
+            @this.SearchRecursively(childrenSelector, predicate, result);
+            return result;
+        }
+
+        public static void SearchRecursively<T>(this IEnumerable<T> @this, Func<T, IEnumerable<T>> childrenSelector, Predicate<T> predicate, List<T> result)
+        {
+            if (@this != null)
+            {
+                foreach (var item in @this)
+                {
+                    if (predicate(item))
+                    {
+                        result.Add(item);
+                    }
+
+                    var children = childrenSelector(item);
+                    children.SearchRecursively(childrenSelector, predicate, result);
+                }
+            }
+        }
     }
 }
---
+++ Tests/JToolbox.Core.Tests/IEnumerableExtensionsTests.cs
@@ -0,0 +1,106 @@
+﻿using JToolbox.Core.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JToolbox.Core.Tests
+{
+    [TestClass]
+    public class IEnumerableExtensionsTests
+    {
+        [TestMethod]
+        public void SearchRecursively_GetItemWithGivenId()
+        {
+            var items = GetItems();
+
+            var id = 6;
+            var foundItems = items.SearchRecursively(x => x.Items, x => x.Id == id);
+
+            Assert.AreEqual(1, foundItems.Count);
+            Assert.AreEqual(id, foundItems.Single().Id);
+        }
+
+        [TestMethod]
+        public void SearchRecursively_GetItemsWithIdsBetween()
+        {
+            var items = GetItems();
+
+            var result = new List<Item>();
+            items.SearchRecursively(x => x.Items, x => x.Id >= 5 && x.Id <= 8, result);
+
+            Assert.AreEqual(4, result.Count);
+        }
+
+        private List<Item> GetItems()
+        {
+            return new List<Item>
+            {
+                new Item
+                {
+                    Id = 1,
+                    Items = new List<Item>
+                    {
+                        new Item
+                        {
+                            Id = 2,
+                            Items = new List<Item>
+                            {
+                                new Item
+                                {
+                                    Id = 3
+                                }
+                            }
+                        },
+                        new Item
+                        {
+                            Id = 4,
+                        },
+                        new Item
+                        {
+                            Id = 5
+                        }
+                    }
+                },
+                new Item
+                {
+                    Id = 6
+                },
+                new Item
+                {
+                    Id = 7,
+                    Items = new List<Item>
+                    {
+                        new Item
+                        {
+                            Id = 8,
+                            Items = new List<Item>
+                            {
+                                new Item
+                                {
+                                    Id = 9
+                                }
+                            }
+                        },
+                        new Item
+                        {
+                            Id = 10,
+                            Items = new List<Item>
+                            {
+                                new Item
+                                {
+                                    Id = 11
+                                }
+                            }
+                        },
+                    }
+                },
+            };
+        }
+
+        private class Item
+        {
+            public int Id { get; set; }
+            public List<Item> Items { get; set; }
+        }
+    }
+}
