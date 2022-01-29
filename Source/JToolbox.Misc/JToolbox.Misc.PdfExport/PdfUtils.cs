436f92ed71f2529c2351591a5c25c46449f6d17e
Jarczyslaw <tyma.jaroslaw@gmail.com>
2022-01-29 16:48:04 +0100

    dev

--- Source/JToolbox.Misc/JToolbox.Misc.PdfExport/PdfUtils.cs
+++ Source/JToolbox.Misc/JToolbox.Misc.PdfExport/PdfUtils.cs
@@ -1,8 +1,12 @@
-﻿using PdfSharp.Pdf;
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering.Printing;
+using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.IO;
+using System.Windows.Forms;
 
 namespace JToolbox.Misc.PdfExport
 {
@@ -48,6 +52,21 @@ namespace JToolbox.Misc.PdfExport
             }
         }
 
+        public static void PrintDocument(Document document)
+        {
+            var printSettings = ShowPrintDialog();
+            if (printSettings == null)
+            {
+                return;
+            }
+
+            var printDocument = new MigraDocPrintDocument(document)
+            {
+                PrinterSettings = printSettings
+            };
+            printDocument.Print();
+        }
+
         private static PdfDocument ImportDocument(PdfDocument document)
         {
             if (document.IsImported)
@@ -60,6 +79,21 @@ namespace JToolbox.Misc.PdfExport
                 document.Save(stream, false);
                 return CompatiblePdfReader.Open(stream, PdfDocumentOpenMode.Import);
             }
+        }
+
+        private static PrinterSettings ShowPrintDialog()
+        {
+            var printerSettings = new PrinterSettings();
+            using (var dialog = new PrintDialog())
+            {
+                dialog.PrinterSettings = printerSettings;
+                dialog.AllowSomePages = true;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    return printerSettings;
+                }
+            }
+            return null;
         }
     }
 }
