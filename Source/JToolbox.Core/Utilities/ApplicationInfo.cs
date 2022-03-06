9d33817318c67fe6f522e48f3beeb84a514dca67
Jarczyslaw <tyma.jaroslaw@gmail.com>
2022-03-06 09:45:47 +0100

    dev

---
+++ Source/JToolbox.Core/Utilities/ApplicationInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace JToolbox.Core.Utilities
+{
+    public static class ApplicationInfo
+    {
+        public static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
