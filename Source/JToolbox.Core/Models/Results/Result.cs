167d9103adfdbdb652daa1a21b7d912cc1c37a93
Jarczyslaw <tyma.jaroslaw@gmail.com>
2022-11-02 19:31:48 +0100

    dev

--- Source/JToolbox.Core/Models/Results/Result.cs
+++ Source/JToolbox.Core/Models/Results/Result.cs
@@ -59,48 +59,5 @@ namespace JToolbox.Core.Models.Results
         }
     }
 
-    public class Result<T> : Result
-    {
-        public Result()
-        {
-        }
-
-        public Result(Result other)
-            : base(other)
-        {
-        }
-
-        public Result(Result<T> other)
-            : base(other)
-        {
-            Value = other.Value;
-        }
-
-        public Result(T value)
-        {
-            Value = value;
-        }
-
-        public T Value { get; set; }
-
-        public new static Result<T> AsError(string error)
-        {
-            var result = new Result<T>();
-            result.Messages.AddError(error);
-            return result;
-        }
-
-        public new static Result<T> AsError(Exception exc)
-        {
-            var result = new Result<T>();
-            result.Messages.AddError(exc);
-            return result;
-        }
-
-        public override void Clear()
-        {
-            base.Clear();
-            Value = default;
-        }
-    }
+    
 }
---
+++ Source/JToolbox.Core/Models/Results/ResultOfT.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace JToolbox.Core.Models.Results
+{
+    public class Result<T> : Result
+    {
+        public Result(Result other)
+            : base(other)
+        {
+        }
+
+        public Result(Result<T> other)
+            : base(other)
+        {
+            Value = other.Value;
+        }
+
+        public Result(T value = default)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public new static Result<T> AsError(string error)
+        {
+            var result = new Result<T>();
+            result.Messages.AddError(error);
+            return result;
+        }
+
+        public new static Result<T> AsError(Exception exc)
+        {
+            var result = new Result<T>();
+            result.Messages.AddError(exc);
+            return result;
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            Value = default;
+        }
+
+        public Result<TResult> OfType<TResult>(TResult value = default)
+        {
+            return new Result<TResult>(this)
+            {
+                Value = value
+            };
+        }
+    }
+}
