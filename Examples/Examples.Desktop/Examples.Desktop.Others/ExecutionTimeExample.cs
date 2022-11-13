b75a1d365dd78556b76240e49ee9079a48dddfe8
Jarczyslaw <tyma.jaroslaw@gmail.com>
2022-11-13 13:05:56 +0100

    dev

--- Examples/Examples.Desktop/Examples.Desktop.Others/ExecutionTimeExample.cs
+++ Examples/Examples.Desktop/Examples.Desktop.Others/ExecutionTimeExample.cs
@@ -1,13 +1,36 @@
 ﻿using Examples.Desktop.Base;
+using JToolbox.Core.Utilities;
+using System;
 using System.Threading.Tasks;
 
 namespace Examples.Desktop.Others
 {
     public class ExecutionTimeExample : BaseExample
     {
-        public override Task Run(IOutputInput outputInput)
+        public override async Task Run(IOutputInput outputInput)
         {
-            return Task.CompletedTask;
+            outputInput.WriteLine("Start");
+
+            ExecutionTime.Start();
+
+            await Wait(1);
+            ExecutionTime.Check("1 second");
+            outputInput.WriteLine("1 second");
+
+            await Wait(2);
+            ExecutionTime.Check("2 seconds");
+            outputInput.WriteLine("2 seconds");
+
+            await Wait(3);
+            ExecutionTime.Check("3 seconds");
+            outputInput.WriteLine("3 seconds");
+
+            outputInput.WriteLine(ExecutionTime.GetChecksResult());
+        }
+
+        private Task Wait(int seconds)
+        {
+            return Task.Delay(TimeSpan.FromSeconds(seconds));
         }
     }
 }
