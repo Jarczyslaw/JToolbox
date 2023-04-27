12dfd080460c9907164a2e4515f820b37bbb5fb8
Jarczyslaw <tyma.jaroslaw@gmail.com>
2023-04-27 21:06:50 +0200

    dev

--- Source/JToolbox.DataAccess/JToolbox.DataAccess.SQLiteNet/Repositories/BaseExtendedRepository.cs
+++ Source/JToolbox.DataAccess/JToolbox.DataAccess.SQLiteNet/Repositories/BaseExtendedRepository.cs
@@ -3,7 +3,6 @@ using JToolbox.DataAccess.Common;
 using JToolbox.DataAccess.SQLiteNet.Entities;
 using SQLite;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace JToolbox.DataAccess.SQLiteNet.Repositories
 {
@@ -11,14 +10,17 @@ namespace JToolbox.DataAccess.SQLiteNet.Repositories
     {
         private readonly ITimeProvider timeProvider;
 
-        public BaseExtendedRepository(ITimeProvider timeProvider)
+        protected BaseExtendedRepository(ITimeProvider timeProvider)
         {
             this.timeProvider = timeProvider;
         }
 
         public virtual bool SafeDelete(SQLiteConnection db, int id)
         {
-            return Update(db, id, e => e.Deleted = true);
+            var where = $"{SqliteExtensions.GetColumnName<BaseExtendedEntity>(nameof(BaseExtendedEntity.Id))} = {id}";
+            var query = CreateSafeDeleteQuery(where);
+
+            return db.Execute(query) > 0;
         }
 
         public virtual bool SafeDelete(SQLiteConnection db, TEntity entity)
@@ -28,14 +30,17 @@ namespace JToolbox.DataAccess.SQLiteNet.Repositories
 
         public virtual void SafeDelete(SQLiteConnection db, List<int> ids)
         {
-            UpdateMany(db, ids, x => x.Deleted = true);
+            var whereIds = string.Join(", ", ids);
+            var where = $"{SqliteExtensions.GetColumnName<BaseExtendedEntity>(nameof(BaseExtendedEntity.Id))} IN ({whereIds})";
+            var query = CreateSafeDeleteQuery(where);
+
+            db.Execute(query);
         }
 
         public virtual void SafeDelete(SQLiteConnection db, List<TEntity> entities)
         {
-            var ids = entities.Select(x => x.Id)
-                .ToList();
-            UpdateMany(db, ids, x => x.Deleted = true);
+            var ids = entities.ConvertAll(x => x.Id);
+            SafeDelete(db, ids);
         }
 
         public virtual void SafeMerge(SQLiteConnection db, List<TEntity> newList, List<TEntity> currentList, IEqualityComparer<TEntity> equalityComparer)
@@ -45,7 +50,7 @@ namespace JToolbox.DataAccess.SQLiteNet.Repositories
 
             foreach (var entity in merge.ToDelete)
             {
-                SafeDelete(db, entity);
+                SafeDelete(db, entity.Id);
             }
 
             if (merge.ToUpdate.Count > 0)
@@ -66,6 +71,16 @@ namespace JToolbox.DataAccess.SQLiteNet.Repositories
             {
                 entity.CreateDate = timeProvider.Now;
             }
+        }
+
+        private string CreateSafeDeleteQuery(string whereCondition)
+        {
+            var query = $@"
+UPDATE {SqliteExtensions.GetTableName<TEntity>()}
+SET {SqliteExtensions.GetColumnName<BaseExtendedEntity>(nameof(BaseExtendedEntity.Deleted))} = 1
+WHERE {whereCondition}";
+
+            return query;
         }
     }
 }
